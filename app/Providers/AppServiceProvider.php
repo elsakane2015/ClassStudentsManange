1def32ef065bccc93a3137e719d1c973c57f690a
aff2018 <aff201805@gmail.com>
2026-01-06 15:26:53 +0800

    更新时区问题

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 强制设置时区 (解决 Docker/Coolify 部署时区问题)
+        date_default_timezone_set('Asia/Shanghai');
+        config(['app.timezone' => 'Asia/Shanghai']);
+        config(['app.locale' => 'zh_CN']);
+        
+        // 设置 Carbon 语言
+        \Carbon\Carbon::setLocale('zh');
+        
+        // 强制 Carbon 序列化为带时区的 ISO 字符串，或者直接给前端 Y-m-d H:i:s
+        // 这里使用 Y-m-d H:i:s 确保前端收到的就是"本地"时间字符串 (15:22)
+        // 这样即使前端解析有误，看到的也是正确的时间数字
+        \Carbon\Carbon::serializeUsing(function ($carbon) {
+            return $carbon->format('Y-m-d H:i:s');
+        });
     }
 }
